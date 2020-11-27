cba2d69f8876444e666b4a21d9ce4ac7d2c38d3b
mrAzay <furoxaz@gmail.com>
2020-11-27 15:39:32 +0200

    footer + aos

--- new/develop/js/main.js
+++ new/develop/js/main.js
@@ -717,7 +717,7 @@ $(document).ready(function () {
     AOS.init({
         once: true,
         duration: 900,
-        offset: 150
+        offset: 550
     });
 
     const
@@ -810,8 +810,8 @@ $(document).ready(function () {
                     <div class="open__item-text day">Last ticket sold one hour prior to closing</div>
                 </div>`;
 
-    oldContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/11mRJZFuJsZhbc2K9">211 S State St, <br>Chicago, IL <br><span>60604</span></a>`;
-    newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 St George St, <br>St. Augustine, FL <br><span>32084</span></a>`;
+    oldContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/11mRJZFuJsZhbc2K9">211 S State St, <br>Chicago, IL <span>60604</span></a>`;
+    newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 St George St, <br>St. Augustine, FL <span>32084</span></a>`;
 
     oldMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5941.280215846711!2d-87.627482!3d41.879089!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2ca33f58ffd1%3A0xf06bc2d1f0d09729!2s211%20S%20State%20St%2C%20Chicago%2C%20IL%2060604%2C%20USA!5e0!3m2!1sen!2sua!4v1597659784049!5m2!1sen!2sua"  width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`;
     newMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6917.892593128432!2d-81.313291!3d29.894648!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e42795ef01c2bd%3A0x8753e82fcf3752db!2s100%20St%20George%20St%2C%20St.%20Augustine%2C%20FL%2032084%2C%20USA!5e0!3m2!1sen!2sua!4v1597659655606!5m2!1sen!2sua"  width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`;
